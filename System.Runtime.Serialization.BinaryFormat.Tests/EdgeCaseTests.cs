e4fa778148302a11d9ce442785be8f18c8fcb368
Adam Sitnik <adam.sitnik@gmail.com>
2024-03-05 20:04:42 +0100

    add edge case tests:
    - cyclic reference
    - surrogates

--- System.Runtime.Serialization.BinaryFormat.Tests/EdgeCaseTests.cs
+++ System.Runtime.Serialization.BinaryFormat.Tests/EdgeCaseTests.cs
@@ -4,7 +4,7 @@ using Xunit;
 
 namespace System.Runtime.Serialization.BinaryFormat.Tests;
 
-public class EdgeCaseTests
+public class EdgeCaseTests : ReadTests
 {
     [Fact]
     public void ThrowsOnInvalidUtf8Input()
@@ -38,6 +38,49 @@ public class EdgeCaseTests
 
         stream.Position = 0;
         Assert.Throws<SerializationException>(() => SafePayloadReader.Read(stream));
+    }
+
+    [Serializable]
+    public class WithCyclicReference
+    {
+        public string? Name;
+        public WithCyclicReference? ReferenceToSelf;
+    }
+
+    [Fact]
+    public void CyclicReferencesAreSupported()
+    {
+        WithCyclicReference input = new();
+        input.Name = "hello";
+        input.ReferenceToSelf = input;
+
+        using MemoryStream stream = Serialize(input);
+
+        ClassRecord classRecord = SafePayloadReader.ReadClassRecord<WithCyclicReference>(stream);
+
+        Assert.Same(classRecord, classRecord[nameof(WithCyclicReference.ReferenceToSelf)]);
+        Assert.Equal(input.Name, classRecord[nameof(WithCyclicReference.Name)]);
+    }
+
+    [Fact]
+    public void SurrogatesGetNoSpecialHandling()
+    {
+#if NETCOREAPP
+        // Type is [Serializable] only on Full .NET Framework.
+        // So here we use a Base64 representation of serialized typeof(object)
+        const string serializedWithFullFramework = "AAEAAAD/////AQAAAAAAAAAEAQAAAB9TeXN0ZW0uVW5pdHlTZXJpYWxpemF0aW9uSG9sZGVyAwAAAAREYXRhCVVuaXR5VHlwZQxBc3NlbWJseU5hbWUBAAEIBgIAAAANU3lzdGVtLk9iamVjdAQAAAAGAwAAAEttc2NvcmxpYiwgVmVyc2lvbj00LjAuMC4wLCBDdWx0dXJlPW5ldXRyYWwsIFB1YmxpY0tleVRva2VuPWI3N2E1YzU2MTkzNGUwODkL";
+
+        using MemoryStream stream = new(Convert.FromBase64String(serializedWithFullFramework));
+#else
+        using MemoryStream stream = Serialize(typeof(object));
+#endif
+
+        ClassRecord classRecord = (ClassRecord)SafePayloadReader.Read(stream);
+
+        // It's a surrogate, so there is no type match.
+        Assert.False(classRecord.IsSerializedInstanceOf(typeof(Type)));
+        Assert.Equal("System.UnitySerializationHolder", classRecord.TypeName);
+        Assert.Equal("mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089", classRecord["AssemblyName"]);
     }
 
     private static void WriteSerializedStreamHeader(BinaryWriter writer, int major, int minor)
