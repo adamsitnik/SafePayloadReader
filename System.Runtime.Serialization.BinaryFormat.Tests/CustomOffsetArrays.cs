14293084449a605a22583f39895f54b34e969d5e
Adam Sitnik <adam.sitnik@gmail.com>
2024-03-11 15:25:10 +0100

    multi dimensional arrays support tests

---
+++ System.Runtime.Serialization.BinaryFormat.Tests/CustomOffsetArrays.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using Xunit;
+
+namespace System.Runtime.Serialization.BinaryFormat.Tests;
+
+public class CustomOffsetArrays : ReadTests
+{
+    [Fact]
+    public void CanReadSingleDimensionalArrayOfIntegersWithCustomOffset()
+    {
+        const int lowerBound = 1;
+        Array arrayOfIntegersIndexedFromOne = Array.CreateInstance(typeof(int),
+            lengths: [3], lowerBounds: [lowerBound]);
+
+        for (int i = lowerBound; i < lowerBound + arrayOfIntegersIndexedFromOne.Length; i++)
+        {
+            arrayOfIntegersIndexedFromOne.SetValue(value: i, index: i); 
+        }
+
+        using MemoryStream stream = Serialize(arrayOfIntegersIndexedFromOne);
+
+        SerializationRecord serializationRecord = SafePayloadReader.Read(stream);
+    }
+
+    [Fact]
+    public void CanReadRectangularArrayOfStringsWithCustomOffsets()
+    {
+        const int lowerBound = 10;
+        Array jaggedArrayOfStringsIndexedFrom10 = Array.CreateInstance(typeof(string),
+            lengths: [7, 5], lowerBounds: [lowerBound, lowerBound]);
+
+        for (int i = lowerBound; i < lowerBound + jaggedArrayOfStringsIndexedFrom10.GetLength(0); i++)
+        {
+            for (int j = lowerBound; j < lowerBound + jaggedArrayOfStringsIndexedFrom10.GetLength(1); j++)
+            {
+                jaggedArrayOfStringsIndexedFrom10.SetValue(value: $"{i}. {j}", index1: i, index2: j);
+            }
+        }
+
+        using MemoryStream stream = Serialize(jaggedArrayOfStringsIndexedFrom10);
+
+        SerializationRecord serializationRecord = SafePayloadReader.Read(stream);
+    }
+}
---
+++ System.Runtime.Serialization.BinaryFormat.Tests/JaggedArraysTests.cs
@@ -0,0 +1,70 @@
+﻿using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace System.Runtime.Serialization.BinaryFormat.Tests;
+
+public class JaggedArraysTests : ReadTests
+{
+    [Fact]
+    public void CanReadJaggedArraysOfPrimitiveTypes()
+    {
+        int[][] array = new int[3][];
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = [i, i, i];
+        }
+
+        using MemoryStream stream = Serialize(array);
+
+        SerializationRecord serializationRecord = SafePayloadReader.Read(stream);
+    }
+
+    [Fact]
+    public void CanReadJaggedArraysOfStrings()
+    {
+        string[][] array = new string[3][];
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = ["a", "b", "c"];
+        }
+
+        using MemoryStream stream = Serialize(array);
+
+        SerializationRecord serializationRecord = SafePayloadReader.Read(stream);
+    }
+
+    [Fact]
+    public void CanReadJaggedArraysOfObjects()
+    {
+        object[][] array = new object[3][];
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = ["a", 1, DateTime.MaxValue];
+        }
+
+        using MemoryStream stream = Serialize(array);
+
+        SerializationRecord serializationRecord = SafePayloadReader.Read(stream);
+    }
+
+    [Serializable]
+    public class ComplexType
+    {
+        public int SomeField;
+    }
+
+    [Fact]
+    public void CanReadJaggedArraysOfComplexTypes()
+    {
+        ComplexType[][] array = new ComplexType[3][];
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = Enumerable.Range(0, i + 1).Select(j => new ComplexType { SomeField = j }).ToArray();
+        }
+
+        using MemoryStream stream = Serialize(array);
+
+        SerializationRecord serializationRecord = SafePayloadReader.Read(stream);
+    }
+}
